0c1f4b0c61f82b337c46a9c38e7f984ddcd260c8
Emircvv7 <spike10lvl@gmail.com>
2023-12-18 16:59:23 +0600

    Initial commit

---
+++ app/src/main/java/com/example/taskmanager/ui/profile/ProfileFragment.kt
@@ -0,0 +1,44 @@
+package com.example.taskmanager.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.taskmanager.databinding.FragmentHomeBinding
+import com.example.taskmanager.databinding.FragmentProfileBinding
+import com.example.taskmanager.ui.home.HomeViewModel
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val ProfileViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.editText
+        ProfileViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
